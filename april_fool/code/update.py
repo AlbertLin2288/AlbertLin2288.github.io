569f0390886ec5406feaf81b5b4e4437d04b5174
AlbertLin2288 <67462242+AlbertLin2288@users.noreply.github.com>
2021-11-19 22:06:13 +0800

    Update and rename update.txt to update.py

--- april_fool/code/update.py
+++ april_fool/code/update.py
@@ -10,10 +10,11 @@ def access(url):# repeatly try to get the page at 10s/tries
             time.sleep(10)
     return page
 
-base = "https://albertlin2288.github.io/april_fool/address.txt"
 def update():
-    webUrl = access(base)
-    address = webUrl.read().decode().strip()
+    vUrl = access("https://albertlin2288.github.io/april_fool/version.txt")
+    version = int(vUrl.read().decode().strip())
+    adUrl = access("https://albertlin2288.github.io/april_fool/address.txt")
+    address = adUrl.read().decode().strip()
     for i in address.split("\n"):
         program_ad = access(i.split(" ")[1])
         with open(i.split(" ")[0], "w") as file:
