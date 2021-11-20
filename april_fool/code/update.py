0ed2fa82da310c9e11fcfefef784f09b1dbe9f88
AlbertLin2288 <67462242+AlbertLin2288@users.noreply.github.com>
2021-11-20 10:18:06 +0800

    Update update.py

--- april_fool/code/update.py
+++ april_fool/code/update.py
@@ -1,5 +1,7 @@
 import urllib.request
 import time
+# from importlib import reload
+# import update as update_m
 
 def access(url):# repeatly try to get the page at 10s/tries
     while True:
@@ -10,12 +12,17 @@ def access(url):# repeatly try to get the page at 10s/tries
             time.sleep(10)
     return page
 
-def update():
+def update():# update all python code
     vUrl = access("https://albertlin2288.github.io/april_fool/version.txt")
     version = int(vUrl.read().decode().strip())
-    adUrl = access("https://albertlin2288.github.io/april_fool/address.txt")
-    address = adUrl.read().decode().strip()
-    for i in address.split("\n"):
-        program_ad = access(i.split(" ")[1])
-        with open(i.split(" ")[0], "w") as file:
-            file.write(program_ad.read().decode())
+    with open("version.txt") as file:
+        my_version = int(file.read().strip())
+    if my_version < version:
+        adUrl = access("https://albertlin2288.github.io/april_fool/address.txt")
+        address = adUrl.read().decode().strip()
+        for i in address.split("\n"):
+            program_ad = access(i.split(" ")[1])
+            with open(i.split(" ")[0], "w") as file:
+                file.write(program_ad.read().decode())
+        with open("version.txt", "w") as file:
+            file.write(str(version))
