f1ec466e2e73b34cb5740761dc856d51068fa293
AlbertLin2288 <67462242+AlbertLin2288@users.noreply.github.com>
2021-11-20 10:20:21 +0800

    Update update.py

--- april_fool/code/update.py
+++ april_fool/code/update.py
@@ -3,25 +3,27 @@ import time
 # from importlib import reload
 # import update as update_m
 
-def access(url):# repeatly try to get the page at 10s/tries
+def access(url, test):# repeatly try to get the page at 10s/tries
     while True:
         try:
             page = urllib.request.urlopen(url)
             break
         except:
+            if test:
+                print("Fail to access " + url)
             time.sleep(10)
     return page
 
-def update():# update all python code
-    vUrl = access("https://albertlin2288.github.io/april_fool/version.txt")
+def update(test =False):# update all python code
+    vUrl = access("https://albertlin2288.github.io/april_fool/version.txt", test)
     version = int(vUrl.read().decode().strip())
     with open("version.txt") as file:
         my_version = int(file.read().strip())
     if my_version < version:
-        adUrl = access("https://albertlin2288.github.io/april_fool/address.txt")
+        adUrl = access("https://albertlin2288.github.io/april_fool/address.txt", test)
         address = adUrl.read().decode().strip()
         for i in address.split("\n"):
-            program_ad = access(i.split(" ")[1])
+            program_ad = access(i.split(" ")[1], test)
             with open(i.split(" ")[0], "w") as file:
                 file.write(program_ad.read().decode())
         with open("version.txt", "w") as file:
